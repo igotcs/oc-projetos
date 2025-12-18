dc83106abab1c57cf10655be617640d2616912c1
Igor Cordeiro <110269278+igotcs@users.noreply.github.com>
2025-12-17 23:05:41 -0300

    finalizado

--- exercicios/exercicio-1.c
+++ exercicios/exercicio-1.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main() {
+    int N = 4;
+
+    int A[4] = {1, 0, 1, 1};  
+    int B[4] = {0, 1, 1, 0};  
+    int C[5];                
+
+    int carry = 0;
+
+    for (int i = N - 1; i >= 0; i--) {
+        int soma = A[i] + B[i] + carry;
+
+        if (soma == 0) {
+            C[i + 1] = 0;
+            carry = 0;
+        } else if (soma == 1) {
+            C[i + 1] = 1;
+            carry = 0;
+        } else if (soma == 2) {
+            C[i + 1] = 0;
+            carry = 1;
+        } else { // soma == 3
+            C[i + 1] = 1;
+            carry = 1;
+        }
+    }
+
+    C[0] = carry;
+
+    printf("Resultado: ");
+    for (int i = 0; i < N + 1; i++) {
+        printf("%d", C[i]);
+    }
+
+    return 0;
+}
